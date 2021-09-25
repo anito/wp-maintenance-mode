de0991484711e5269cf61f98a0bf68e6c01e45fe
anito <axelnitzschner@mac.com>
2021-09-25 22:45:14 +0200

    3.2.7

--- views/maintenance.php
+++ views/maintenance.php
@@ -8,21 +8,21 @@
         <meta name="keywords" content="<?php echo esc_attr($keywords); ?>" />
         <meta name="robots" content="<?php echo esc_attr($robots); ?>" />
 		<?php
-		if (!empty($styles) && is_array($styles)) {
-			foreach ($styles as $src) {
-				?>
+if (!empty($styles) && is_array($styles)) {
+    foreach ($styles as $src) {
+        ?>
 				<link rel="stylesheet" href="<?php echo $src; ?>">
 				<?php
-			}
-		}
-		if (!empty($custom_css) && is_array($custom_css)) {
-			echo '<style>' . implode(array_map('stripslashes', $custom_css)) . '</style>';
-		}
+}
+}
+if (!empty($custom_css) && is_array($custom_css)) {
+    echo '<style>' . implode(array_map('stripslashes', $custom_css)) . '</style>';
+}
 
-		// do some actions
-		do_action('wm_head'); // this hook will be removed in the next versions
-		do_action('wpmm_head');
-		?>
+// do some actions
+do_action('wm_head'); // this hook will be removed in the next versions
+do_action('wpmm_head');
+?>
     </head>
     <body class="<?php echo $body_classes ? $body_classes : ''; ?>">
 		<?php do_action('wpmm_after_body'); ?>
@@ -30,12 +30,12 @@
         <div class="wrap">
 			<?php if (!empty($heading)) { ?><h1><?php echo stripslashes($heading); ?></h1><?php } ?>
 
-			<?php 
-				// If bot is enabled no text will be shown
-				if ( !empty($text) && $this->plugin_settings['bot']['status'] === 0) {
-					echo "<h2>" . stripslashes($text) . "</h2>";
-				}
-			?>
+			<?php
+// If bot is enabled no text will be shown
+if (!empty($text) && $this->plugin_settings['bot']['status'] === 0) {
+    echo "<h2>" . stripslashes($text) . "</h2>";
+}
+?>
 
 
             <?php if (!empty($this->plugin_settings['bot']['status']) && $this->plugin_settings['bot']['status'] === 1) { ?>
@@ -57,15 +57,15 @@
             <?php } ?>
 
 			<?php
-			if (!empty($this->plugin_settings['modules']['countdown_status']) && $this->plugin_settings['modules']['countdown_status'] == 1) {
-				?>
+if (!empty($this->plugin_settings['modules']['countdown_status']) && $this->plugin_settings['modules']['countdown_status'] == 1) {
+    ?>
 				<div class="countdown" data-start="<?php echo date('F d, Y H:i:s', strtotime($countdown_start)); ?>" data-end="<?php echo date('F d, Y H:i:s', $countdown_end); ?>"></div>
 			<?php } ?>
 
 			<?php if (!empty($this->plugin_settings['modules']['subscribe_status']) && $this->plugin_settings['modules']['subscribe_status'] == 1
-                      // If the bot is active, legacy subscribe form will be hidden
-                      // !empty($this->plugin_settings['bot']['status']) && 
-                      && $this->plugin_settings['bot']['status'] === 0 ) { ?>
+    // If the bot is active, legacy subscribe form will be hidden
+    // !empty($this->plugin_settings['bot']['status']) &&
+    && $this->plugin_settings['bot']['status'] === 0) { ?>
 				<?php if (!empty($this->plugin_settings['modules']['subscribe_text'])) { ?><h3><?php echo stripslashes($this->plugin_settings['modules']['subscribe_text']); ?></h3><?php } ?>
 				<div class="subscribe_wrapper" style="min-height: 100px;">
 					<form class="subscribe_form">
@@ -75,7 +75,7 @@
 						</div>
 						<?php if (!empty($this->plugin_settings['gdpr']['status']) && $this->plugin_settings['gdpr']['status'] == 1) { ?>
 							<div class="privacy_checkbox"><input type="checkbox" name="acceptance" value="YES" data-rule-required="true" data-msg-required="<?php esc_attr_e('This field is required.', $this->plugin_slug); ?>"><label for="acceptance"><?php _e("I've read and agree with the site's privacy policy", $this->plugin_slug); ?></label></div>
-							<?php if(!empty($this->plugin_settings['gdpr']['subscribe_form_tail'])) { ?>
+							<?php if (!empty($this->plugin_settings['gdpr']['subscribe_form_tail'])) { ?>
 								<p class="privacy_tail"><?php echo $this->plugin_settings['gdpr']['subscribe_form_tail']; ?></p>
 						<?php }} ?>
 					</form>
@@ -136,7 +136,7 @@
 
 							<?php if (!empty($this->plugin_settings['gdpr']['status']) && $this->plugin_settings['gdpr']['status'] == 1) { ?>
 								<div class="privacy_checkbox"><input type="checkbox" name="acceptance" value="YES" data-rule-required="true" data-msg-required="<?php esc_attr_e('This field is required.', $this->plugin_slug); ?>"><label for="acceptance"><?php _e("I've read and agree with the site's privacy policy", $this->plugin_slug); ?></label></div>
-								<?php if(!empty($this->plugin_settings['gdpr']['contact_form_tail'])) { ?>
+								<?php if (!empty($this->plugin_settings['gdpr']['contact_form_tail'])) { ?>
 									<p class="privacy_tail"><?php echo $this->plugin_settings['gdpr']['contact_form_tail']; ?></p>
 								<?php }} ?>
 							<p class="submit"><input type="submit" value="<?php _e('Send', $this->plugin_slug); ?>"></p>
@@ -150,10 +150,10 @@
 			<?php } ?>
 
 			<?php if ((!empty($this->plugin_settings['general']['admin_link']) && $this->plugin_settings['general']['admin_link'] == 1) ||
-					  (!empty($this->plugin_settings['gdpr']['status']) && $this->plugin_settings['gdpr']['status'] == 1)) { ?>
+    (!empty($this->plugin_settings['gdpr']['status']) && $this->plugin_settings['gdpr']['status'] == 1)) { ?>
 				<div class="author_link">
-					<?php if($this->plugin_settings['general']['admin_link'] == 1) { ?>
-						<a href="<?php echo admin_url(); ?>"><?php _e('Dashboard', $this->plugin_slug); ?></a> 
+					<?php if ($this->plugin_settings['general']['admin_link'] == 1) { ?>
+						<a href="<?php echo admin_url(); ?>"><?php _e('Dashboard', $this->plugin_slug); ?></a>
 					<?php } ?>
 					<?php if ($this->plugin_settings['gdpr']['status'] == 1) { ?>
 						<a href="<?php echo $this->plugin_settings['gdpr']['policy_page_link']; ?>"><?php echo $this->plugin_settings['gdpr']['policy_page_label']; ?></a>
@@ -167,21 +167,21 @@
 		</script>
 
 		<?php
-		
-		// Hook before scripts, mostly for internationalization
-		do_action('wpmm_before_scripts');
 
-		if (!empty($scripts) && is_array($scripts)) {
-			foreach ($scripts as $src) {
-				?>
+// Hook before scripts, mostly for internationalization
+do_action('wpmm_before_scripts');
+
+if (!empty($scripts) && is_array($scripts)) {
+    foreach ($scripts as $src) {
+        ?>
 				<script src="<?php echo $src; ?>"></script>
 				<?php
-			}
-		}
-		// Do some actions
-		do_action('wm_footer'); // this hook will be removed in the next versions
-		do_action('wpmm_footer');
-		?>
+}
+}
+// Do some actions
+do_action('wm_footer'); // this hook will be removed in the next versions
+do_action('wpmm_footer');
+?>
         <?php if (!empty($this->plugin_settings['bot']['status']) && $this->plugin_settings['bot']['status'] === 1) { ?>
             <script type='text/javascript'>
                 jQuery(function($) {
